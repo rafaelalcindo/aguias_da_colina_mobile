b7d69b01b6d0abd9f9d4eee28a5342dea479f766
Rafael Alcindo <rafael_alcindo@hotmail.com>
2020-12-04 22:58:25 -0300

    começo do redux com duck pattern

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StatusBar, View } from 'react-native'
-
+import './src/config/ReactotronConfig'
 import Routes from './src/routes'
 
 export default function App() {
